881f40df47edf8e8a2959802e230a0498742545c
jparrales <jparrales@chiang.ec>
2020-04-22 23:29:00 -0500

    jparrales-22-04-2020 Agregando cambios para primer pantalla de login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -7,60 +7,104 @@
 				</a>
 			</template>
 		</b-navbar>
-		<div class="columns body is-mobile is-vcentered">
-			<div class="column"></div>
-			<div class="column is-two-fifths-desktop is-four-fifths-touch">
-				<div class="box">
-					<article class="media">
+		<div class="columns body is-paddingless">
+			<div class="column is-full is-paddingless">
+				<img src="../assets/Artboard_242_1.png" />
+			</div>
+		</div>
+		<div class="columns is-mobile is-vcentered is-paddingless height-login">
+			<div class="column artboard is-paddingless">
+				<img class="is-pulled-right" src="../assets/inicio_plan1_1.png" />
+			</div>
+			<div class="column is-three-fifths-desktop is-four-fifths-touch login_main">
+				<div class="box is-paddingless height-login">
+					<article class="media no-flex">
 						<div class="media-content">
 							<div class="content">
-								<div class="columns is-centered">
-									<div class="column"></div>
-									<div class="column is-8">
-										<img src="../assets/happy.png" />
-									</div>
-									<div class="column"></div>
-								</div>
-								<p class="title has-text-primary has-text-centered">
-									Ingresa a Kronio
-								</p>
-								<div class="columns is-centered">
-									<div class="column"></div>
-									<div class="column is-8">
-										<section>
-											<b-field>
-												<b-input
-													v-model="login_form.username"
-													@keyup.enter.native="login()"
-													placeholder="Usuario"
-												></b-input>
-											</b-field>
+								<div class="columns is-centered height-login">
+									<div class="column height-login border-right padding-top-8 is-vcentered">
+										<p class="subtitle is-3 titles-login has-text-primary has-text-centered">
+											Iniciar sesión
+										</p>
+										<div class="columns is-centered">
+											<div class="column"></div>
+											<div class="column is-8">
+												<section class="margin-button">
+													<b-field>
+														<p class="control has-icons-left has-icons-right">
+															<b-input class="input-style"
+																v-model="login_form.username"
+																@keyup.enter.native="login()"
+																placeholder="Usuario">
+															</b-input>
+															<span class="icon is-small is-left">
+																<i class="fas fa-user"></i>
+															</span>
+														</p>
+													</b-field>
 
-											<b-field>
-												<b-input
-													v-model="login_form.password"
-													@keyup.enter.native="login()"
-													type="password"
-													placeholder="Contraseña"
-													password-reveal
-												>
-												</b-input>
-											</b-field>
-											<b-field>
-												<b-button @click="login()" type="is-primary" expanded
-													>Ingresar</b-button
-												>
-											</b-field>
-										</section>
+													<b-field>
+														<p class="control has-icons-left has-icons-right">
+															<b-input class="input-style"
+																v-model="login_form.password"
+																@keyup.enter.native="login()"
+																type="password"
+																placeholder="Contraseña">
+															</b-input>
+															<span class="icon is-small is-left">
+																<i class="fas fa-lock"></i>
+															</span>
+														</p>
+													</b-field>
+													<b-flied>
+														<label class="checkbox is-pulled-right input-style-remember">
+															<input type="checkbox">
+															Recuérdame
+														</label>
+													</b-flied>
+													<b-field>
+														<b-button @click="login()" type="is-primary" expanded class="btn-login body">Ingresar</b-button>
+													</b-field>
+													<b-flied>
+														<a class="forgot-pass is-pulled-right" href="#">¿Olvidó contraseña?</a>
+													</b-flied>
+												</section>
+											</div>
+											<div class="column"></div>
+										</div>
 									</div>
-									<div class="column"></div>
+									<div class="column height-login border-left padding-top-8 is-vcentered">
+										<p class="subtitle is-3 titles-login has-text-primary has-text-centered">
+											Registro
+										</p>
+										<div class="columns is-centered">
+											<div class="column"></div>
+											<div class="column is-8">
+												<section>
+													<p class="title is-5 has-text-centered input-style margin-label-register">¿Nuevo en Kronio?</p>
+													<b-field>
+														<b-button @click="login()" type="is-primary" expanded class="btn-login">Regístrate</b-button>
+													</b-field>
+												</section>
+											</div>
+											<div class="column"></div>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</article>
 				</div>
 			</div>
-			<div class="column"></div>
+			<div class="column is-paddingless">
+				<img class="is-pulled-left" src="../assets/inicio_plan2_1.png" />
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column is-full is-has-center margin-text-end">
+				<p class="title is-4 has-text-primary is-paddingless is-marginless">¡Comienza tu prueba gratis hoy!</p>
+				<p class="title is-6 has-text-primary is-paddingless is-marginless">Puedes cancelar en cualquier momento.</p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -102,7 +146,31 @@ export default class Login extends PageBase {
 </script>
 
 <style lang="scss">
+body{
+	font-family: Poppins !important;
+	font-style: normal !important;
+}
+
 .login {
+	.input-style{
+		color: rgba(122, 121, 121, 0.5);
+		margin-top: 7% !important;
+		margin-bottom: 7% !important;
+	}
+
+	.input-style-remember{
+		color: rgba(122, 121, 121, 0.5);
+		margin-bottom: 4% !important;
+	}
+
+	.border-right{
+		border-right: 1px solid rgba(122, 121, 121, 0.3);
+	}
+
+	.border-left{
+		border-left: 1px solid rgba(122, 121, 121, 0.3);
+	}
+
 	.navbar {
 		background: #8969eb;
 		box-shadow: 0 3px 3px rgba(0, 0, 0, 0.05);
@@ -140,12 +208,54 @@ export default class Login extends PageBase {
 	}
 
 	.body {
-		margin-top: 4rem;
-		height: calc(100vh - 4rem);
+		margin-top: 6%;
+	}
+
+	.margin-text-end{
+		margin-top: 3%;
+	}
+
+	.height-login{
+		height: calc(60vh - 4rem);
+	}
+
+	.titles-login{
+		font-weight: normal;
 	}
 
 	.navbar-burger {
 		display: none;
+	}
+
+	.login-main{
+		background: #FFFFFF;
+		box-shadow: 0px 6px 30px rgba(0, 0, 0, 0.15);
+		border-radius: 5px;
+	}
+
+	.artboard {
+    	margin-bottom: 0px !important;
+	}
+
+	.padding-top-8{
+		padding-top: 8%;
+	}
+	.btn-login{
+		background: #335EEA;
+		border-radius: 3px;
+	}
+
+	.margin-label-register{
+		margin-top: 10% !important;
+		margin-bottom: 30% !important;
+	}
+
+	.no-flex{
+		display: inline !important;
+	}
+
+	.forgot-pass{
+		color: #335EEA;
 	}
 }
 </style>
