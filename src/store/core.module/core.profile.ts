96d986ad5b9deb169dd93dcf27366f6d96883c1a
Juan Bonoso <app@kronio.io>
2020-05-04 18:43:17 -0500

    Estados de CoreProfile y mutaciones agregadas

--- src/store/core.module/core.profile.ts
+++ src/store/core.module/core.profile.ts
@@ -2,6 +2,7 @@ import { createModule, mutation, action } from 'vuex-class-component';
 import axios from 'axios';
 import config from '../config.store';
 import { vxm } from '@/store';
+import { ProfileData } from '../types/coreProfileObjects';
 
 const CoreUrl = {
 	profile: config.Kronio + 'public/profile',
@@ -19,5 +20,29 @@ const VuexModule = createModule({
 });
 
 export default class CoreProfileStore extends VuexModule {
+	public profiles: ProfileData[] = [];
+	public employee_email: string = null;
+	public employeesProfileList: ProfileData[] = [];
 
+	@mutation set_profile_id(data) {
+		this.profiles = [new ProfileData(data)];
+	}
+
+	@mutation set_all_profiles_from_current_user(data) {
+		if (Array.isArray(this.profiles) && this.profiles.length > 0) {
+			this.profiles = [];
+		}
+		data.forEach(e => {
+			this.profiles.push(new ProfileData(e));
+		});
+	}
+
+	@mutation set_employee_profile_list(data) {
+		if (Array.isArray(this.employeesProfileList) && this.employeesProfileList.length > 0) {
+			this.employeesProfileList = [];
+		}
+		data.forEach(e => {
+			this.employeesProfileList.push(new ProfileData(e));
+		});
+	}
 }
