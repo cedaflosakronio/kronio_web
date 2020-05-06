7dd72612934a9ebba0866d82a4a7ee0199ee4287
Juan Bonoso <app@kronio.io>
2020-05-06 15:23:28 -0500

    importación del módulo managework a a vxm

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,8 @@ import Vuex from 'vuex';
 import { extractVuexModule, createProxy } from 'vuex-class-component';
 
 import AuthStore from './auth.module/auth.store';
-import GeneralProfile from './core.module/core.profile';
+import CoreProfileStore from './core.module/core.profile';
+import CoreManageWorkStore from './core.module/core.managework';
 import EnterpriseStore from './enterprise.module/enterprise.store';
 import navigationProps from './navigation.module/navigationProps';
 
@@ -12,7 +13,8 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
 	modules: {
 		...extractVuexModule(AuthStore),
-		...extractVuexModule(GeneralProfile),
+		...extractVuexModule(CoreProfileStore),
+		...extractVuexModule(CoreManageWorkStore),
 		...extractVuexModule(EnterpriseStore),
 		...extractVuexModule(navigationProps),
 	},
@@ -20,7 +22,8 @@ export const store = new Vuex.Store({
 
 export const vxm = {
 	auth: createProxy(store, AuthStore),
-	core_profile: createProxy(store, GeneralProfile),
+	core_profile: createProxy(store, CoreProfileStore),
+	core_manageWork: createProxy(store, CoreManageWorkStore),
 	enterprise: createProxy(store, EnterpriseStore),
 	navigation_props: createProxy(store, navigationProps),
 };
