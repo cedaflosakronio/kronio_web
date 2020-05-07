f5570e9a9e826cb5b8392f384312de3832645e1c
Juan Bonoso <app@kronio.io>
2020-05-07 18:46:57 -0500

    Enumeracion realizada a las paginas de vue router

---
+++ src/utils/enums/Pages.ts
@@ -0,0 +1,12 @@
+export enum PAGE_PATH {
+	LOGIN = '/login',
+	REGISTER_ADMIN_PERSON = '/registerAdminperson',
+	REGISTER_ADMIN_PERSON_MORE = '/registerAdminpersonmore',
+	REGISTER_ADMIN_ENTERPRISE = '/registerAdminenterprise',
+	REGISTER_ADMIN_ENTERPRISE_MORE = '/registerAdminenterprisemore',
+	REGISTER_INVITATION_EMPLOYEE = '/registerinvitationEmployee',
+	REGISTER_EMPLOYEE_PERSON = '/registerEmployeePerson',
+	REGISTER_EMPLOYEE_PERSON_MORE = '/registerEmployeePersonmore',
+	FORGOT_PASS_MAIN = '/forgotpassmain',
+	FORGOT_PASS_CODE = '/forgotpasscode',
+}
