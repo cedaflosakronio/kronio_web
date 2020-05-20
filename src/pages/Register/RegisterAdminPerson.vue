98782bb5959c8f5efcc851ec84bfdc6fceaa9502
jparrales <jparrales@chiang.ec>
2020-05-19 22:28:30 -0500

    jparrales-18-05-2020 agregando css para media queries de login y RegisterAdminPerson

--- src/pages/Register/RegisterAdminPerson.vue
+++ src/pages/Register/RegisterAdminPerson.vue
@@ -13,8 +13,8 @@
 			</template>
 		</b-navbar>
 		<div class="columns margin-top-register">
-			<div class="column is-3"></div>
-			<div class="column is-3 is-mobile is-vcentered margin-top-form">
+			<div class="column is-1"></div>
+			<div class="column is-5 is-centered">
 				<div class="box margin-top-form">
 					<article class="media">
 						<div class="media-content">
@@ -141,6 +141,12 @@ body {
 	font-family: Poppins !important;
 	font-style: normal !important;
 }
+
+.no-is-pulled-right {
+	@media screen and (max-width: 768px) {
+		float: none !important;
+	}
+}
 .login {
 	.navbar {
 		background: #8969eb;
@@ -187,8 +193,28 @@ body {
 	}
 
 	.margin-top-form {
-		margin-top: 6%;
-		height: calc(65vh - 4rem);
+		margin-top: 12%;
+		width: 70%;
+		margin-left: 10%;
+		height: calc(80vh - 4rem);
+
+		@media screen and (max-width: 1400px) {
+			width: 75%;
+			margin-top: 15%;
+			height: calc(61vh - 4rem);
+		}
+
+		@media screen and (max-width: 1080px) {
+			width: 70%;
+			margin-top: 3%;
+			height: calc(61vh - 4rem);
+		}
+
+		@media screen and (max-width: 768px) {
+			width: 90%;
+			margin-top: 3%;
+			height: calc(70vh - 4rem);
+		}
 	}
 
 	.title-register {
