f2d06b55981e86a57fffda473090a6cbb141032d
Julio Alvia <jealvia@pop-os.localdomain>
2020-05-31 16:21:19 -0500

    jealvia 31-05-2020 Detalles de responsive

--- src/pages/MainApp/Home.vue
+++ src/pages/MainApp/Home.vue
@@ -18,7 +18,7 @@
 				</span>
 			</button>
 			<div class="content-options-employee" v-if="showOptionsEmployee">
-				<div class="columns" @click="goTo(3)">
+				<div class="columns is-mobile" @click="goTo(3)">
 					<div class="column">
 						<label class="label label-employee">Empleados</label>
 					</div>
@@ -26,7 +26,7 @@
 						<i class="fas fa-plus-circle"></i>
 					</div>
 				</div>
-				<div class="columns" @click="goTo(5)">
+				<div class="columns is-mobile" @click="goTo(5)">
 					<div class="column">
 						<label class="label label-assistance">Asistencia</label>
 					</div>
